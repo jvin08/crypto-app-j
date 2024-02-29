9df7939d9071cef5d3ef6e4041df87c0a595348f
Ivan Vee <jvin08@gmail.com>
2024-02-29 14:26:16 -0600

    setting-up-time-component

---
+++ app/components/coinsConvertor/Time.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import clsx from "clsx";
+const Time = ({darkmode}: {darkmode: boolean}) => {
+    const dateTime = new Date();
+    const formattedDateTime = dateTime.toLocaleString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false // Use 24-hour format
+    });
+    return (
+        <div className={clsx("text-base",{
+            "text-cryptoblue-810 opacity-[.8]": !darkmode,
+            "text-cryptodark-510": darkmode,
+        })}>
+            {formattedDateTime}
+        </div>
+    );
+};
+export default Time;
