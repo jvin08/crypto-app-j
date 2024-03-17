6c4312d5fbbf13b7d6aff8ffcac94350f915cc71
Ivan Vee <jvin08@gmail.com>
2024-03-17 18:32:01 -0500

    added-notifications-to-convertor-button

--- app/components/coinsConverter/ButtonsBox.tsx
+++ app/components/coinsConverter/ButtonsBox.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import CustomButton from "./CustomButton";
-import { useSelector } from "react-redux";
-import { selectDarkmode } from "@/app/lib/dynamicValuesSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectDarkmode, setShowNotification, setNotification } from "@/app/lib/dynamicValuesSlice";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 const ButtonsBox = () => {
+  const dispatch = useDispatch();
+  const handleNotification = (message: string) => {
+    dispatch(setNotification(message));
+    dispatch(setShowNotification(""));
+  };
   const pathname = usePathname();
   const active = pathname === "/" ? false : true;
   const darkmode = useSelector(selectDarkmode);
   return (
-    <div className={clsx("p-1 rounded-md flex w-fit", {
+    <div className={clsx("mt-24 p-1 rounded-md flex w-fit", {
       "bg-cryptoblue-100": !darkmode,
       "bg-cryptodark-150": darkmode,
     })}>
-      <Link href="/"><CustomButton name="Coins"  handleClick={()=>{}} active={active} width="w-64" padding="py-2"/></Link>
-      <Link href="/converter"><CustomButton name="Convertor" handleClick={()=>{}} active={!active} width="w-64" padding="py-2" /></Link>
+      <Link href="/"><CustomButton name="Coins" handleClick={()=>handleNotification("Coins on")} active={active} width="w-64" padding="py-2"/></Link>
+      <Link href="/converter"><CustomButton name="Converter" handleClick={()=>handleNotification("Converter on")} active={!active} width="w-64" padding="py-2" /></Link>
     </div>
   );
 };
