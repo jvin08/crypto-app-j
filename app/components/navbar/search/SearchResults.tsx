ca420fb39fb353e5179c52d620515e21cc5765e1
Ivan Vee <jvin08@gmail.com>
2024-04-21 17:43:15 -0500

    search-bar-improvements

--- app/components/navbar/search/SearchResults.tsx
+++ app/components/navbar/search/SearchResults.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from "react";
-import Image from "next/image";
-import { useDispatch, useSelector } from "react-redux";
-import { useGetSearchCoinsDataQuery, useGetTenCoinsPricesQuery } from "@/app/lib/marketSlice";
-import { selectDarkmode, selectCoinOneSymbol, setCoinOneSymbol, setCoinTwoSymbol, selectCurrency } from "@/app/lib/dynamicValuesSlice";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import LoadingSearch from "./LoadingSearch";
+import { useGetSearchCoinsDataQuery } from "@/app/lib/marketSlice";
+import { selectDarkmode } from "@/app/lib/dynamicValuesSlice";
+import { useClickOutside } from "../../portfolioModal/hooks";
 import clsx from "clsx";
+import path from "path";
 type Coin = {
   name: string;
   id: string;
@@ -23,27 +26,12 @@ const DropdownSearch = ({
     index: number,
     handleCoin: any
   }) => {
-  const dispatch = useDispatch();
-  const currency = useSelector(selectCurrency);
   const { data, isLoading } = useGetSearchCoinsDataQuery(query);
   const darkmode = useSelector(selectDarkmode);
-  const coinOne = useSelector(selectCoinOneSymbol);
   const coinsForRender = data?.coins.slice(0,10);
-  const queryTenCoins = coinsForRender?.map((coin: Coin) => coin.id).join("%2C");
-  const { data: prices } = useGetTenCoinsPricesQuery(queryTenCoins, currency);
-  const pricesStore = {} as {[key: string]: [number, boolean, string]};
-  prices && Object.entries(prices).map((price) => {
-    const coinSymbol = data?.coins.filter((coin: Coin) => coin.id === price[0])[0]?.symbol;
-    const goingUp = prices[price[0]][`${currency.label.toLowerCase()}_24h_change`] > 0;
-    pricesStore[price[0]] = [prices[price[0]][currency.label.toLowerCase()], goingUp, coinSymbol];
-  });
   const newIndex = index % coinsForRender?.length; 
-  const ref = useRef<HTMLDivElement>(null);
   const itemRef = useRef<HTMLDivElement>(null);
-  const handleClickCoin = (e: React.MouseEvent<HTMLDivElement>, coin: Coin) => {
-    e.preventDefault();
-    dispatch(setCoinOneSymbol([coin.id, coin.symbol]));
-    dispatch(setCoinTwoSymbol(coinOne));
+  const closeDropdownMenu = () => {
     toggleHidden();
     clearSearch();
   };
@@ -51,62 +39,38 @@ const DropdownSearch = ({
     if(itemRef.current){
       handleCoin(data?.coins[newIndex]?.id, data?.coins[newIndex]?.symbol);
     }
-  },[newIndex, handleCoin, data]);
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        toggleHidden();
-        clearSearch();
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  },[toggleHidden, clearSearch]);
+  },[newIndex, data]);
+  const dropDownRef = useClickOutside(()=>{
+    closeDropdownMenu();
+  });
   return (
-    <div className={clsx("box-border text-sm w-72 left-0 top-[47px] absolute z-50 rounded-md",{
-      "bg-cryptoblue-200 text-cryptoblue-500": !darkmode,
-      "bg-cryptodark-200 text-cryptodark-100 border-cryptodark-170 border-[1px]": darkmode,
-    })} ref={ref}>
-      {coinsForRender?.length === 0 || isLoading 
-        ? <div 
-          className={clsx("flex items-center h-12 pl-3 py-1 rounded-md",{
-            "bg-cryptoblue-200": !darkmode,
-            "bg-cryptodark-200": darkmode,
-          })}
-        >
-          <p className="ml-9 mr-2 text-xs">Loading...</p>
-        </div>
-        : coinsForRender?.map((coin: Coin, idx: number) => {
-          const priceGoingUp = pricesStore[coin.id]?.[1];
+    coinsForRender?.length === 0 || isLoading ? <LoadingSearch closeLoader={closeDropdownMenu} />
+      : <div className={clsx("box-border text-sm w-72 left-0 top-[47px] absolute z-50 rounded-b-md",{
+        "bg-cryptoblue-200 text-cryptoblue-500": !darkmode,
+        "bg-gradient-to-r from-cryptodark-200 to-dark-140 text-cryptodark-100 border-cryptodark-170 border-[1px]": darkmode,
+      })} ref={dropDownRef}>
+        {coinsForRender?.map((coin: Coin, idx: number) => {
           return (
-            <div key={coin.id} className="first:pt-2 p-1">
-              <div 
-                className={clsx("flex items-center pl-3 py-1 rounded-md hover:rounded-md",{
-                  "hover:bg-cryptodark-400": darkmode,
-                  "hover:bg-cryptodark-100": !darkmode,
-                  "bg-cryptodark-400": idx === newIndex && darkmode,
-                  "bg-cryptoblue-100": idx === newIndex && !darkmode,
-                })}
-                onClick={(e:React.MouseEvent<HTMLDivElement>)=>handleClickCoin(e, coin)} 
-                tabIndex={0}
-                ref={idx === newIndex ? itemRef : null}
-              >
-                {coin.thumb.includes("https") && <Image src={coin.thumb} alt={coin.name} width={20} height={20} className="mr-3"/>}
-                <p className="truncate pr-4">{coin.name}</p>
-                <svg className="ml-auto mr-0" transform={priceGoingUp ? "rotate(0)" : "rotate(180)"} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M8.00065 6.33301L4.66732 9.66634H11.334L8.00065 6.33301Z" fill={priceGoingUp ? "#00B1A7" : "red"} fillOpacity={1}/>
-                </svg>
-                <p className={clsx("ml-1 mr-2 text-[0.65rem]",{
-                  "text-cryptoblue-650": priceGoingUp,
-                  "text-cryptoblue-750": !priceGoingUp,
-                })}>{pricesStore[coin.id]?.[0]?.toFixed(3)}</p>
+            <Link href={path.join("/coin", coin.id) + "market"} key={coin.id}>
+              <div className="first:pt-2 p-1">
+                <div 
+                  className={clsx("flex items-center pl-3 py-1 rounded-md hover:rounded-md",{
+                    "hover:bg-cryptodark-400": darkmode,
+                    "hover:bg-cryptodark-100": !darkmode,
+                    "bg-cryptodark-400": idx === newIndex && darkmode,
+                    "bg-cryptoblue-100": idx === newIndex && !darkmode,
+                  })}
+                  tabIndex={0}
+                  ref={idx === newIndex ? itemRef : null}
+                  onClick={closeDropdownMenu}
+                >
+                  <p className="truncate pl-7">{coin.name}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
-    </div>
+      </div>
   );
 };
 export default DropdownSearch;
