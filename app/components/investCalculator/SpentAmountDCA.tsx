0889669584cc92a9a8cabf1fdfd43ed96e97f966
Ivan Vee <jvin08@gmail.com>
2024-05-17 20:17:57 -0500

    Update SpentAmountDCA.tsx

--- app/components/investCalculator/SpentAmountDCA.tsx
+++ app/components/investCalculator/SpentAmountDCA.tsx
@@ -24,15 +24,15 @@ const SpentAmountDCA = (
   }) => {
   const { data } = useGetCoinDataByDateQuery(query);
   const coinPrices = filterPrices(data, days, interval, startTime, endTime);
-  const spentMoney = amountInvestedDCA(initialAmount, coinPrices, growAmount);
-  const chartData = spentMoney[2] as number[][];
-  useEffect(()=>{
+  const define = amountInvestedDCA(initialAmount, coinPrices, growAmount);
+  const chartData = define.investValuePerPeriod as number[][];
+  useEffect(() => {
     getChartData(chartData);
   },[chartData]);
   return (
     <>
-      <p className="pt-[14px] h-[52px] text-right border-b-[1px] border-cryptodark-160">{spentMoney[0]}</p>
-      <p className="pt-[14px] h-[52px] text-right">{spentMoney[1]}</p>
+      <p className="pt-[14px] h-[52px] text-right border-b-[1px] border-cryptodark-160">{define.spentAmount}</p>
+      <p className="pt-[14px] h-[52px] text-right">{define.lastValue}</p>
     </>
   );
 };
