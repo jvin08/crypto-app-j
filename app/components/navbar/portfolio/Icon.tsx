b520725c89d1f944fdf2bef639634a4a99269283
Ivan Vee <jvin08@gmail.com>
2024-05-26 14:39:55 -0500

    Update Icon.tsx

--- app/components/navbar/portfolio/Icon.tsx
+++ app/components/navbar/portfolio/Icon.tsx
@@ -12,7 +12,7 @@ export const SmallIcon = ({active,darkmode}:{active:boolean,darkmode:boolean}) =
     </svg>
   );
 };
-const Icon = ({active,darkmode}:{active:boolean,darkmode:boolean}) => {
+export const Icon = ({active,darkmode}:{active:boolean,darkmode:boolean}) => {
   return (
     <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M21.5 12L12.5 18L3.5 12M21.5 16L12.5 22L3.5 16M21.5 8L12.5 14L3.5 8L12.5 2L21.5 8Z" 
@@ -24,4 +24,3 @@ const Icon = ({active,darkmode}:{active:boolean,darkmode:boolean}) => {
     </svg>
   );
 };
-export default Icon;
